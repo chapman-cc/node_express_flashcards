32401806ea101393776149021aba75365bf0dc5c
chapmanCC <chapboy1989@gmail.com>
2019-05-30 02:39:33 +0800

    section 1 fin

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send("Hello")
+app.get('/', (req, res) => {
+    res.send("<h1>I luv Treehouse!</h1>");
 })
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+    res.send("<h1>Hello World</h1>");
+})
+
+app.listen(3000, () => {
+    console.log("the application is running on localhost:3000");
+    
+});
