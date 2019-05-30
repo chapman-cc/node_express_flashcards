93c483f2b33a4da54bfbf236cc0a7c17f81cbcca
chapmanCC <chapboy1989@gmail.com>
2019-05-30 08:47:32 +0800

    bump

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send("<h1>I luv Treehouse!</h1>");
+    res.render('index');
 })
 
 app.get('/hello', (req, res) => {
@@ -11,6 +13,5 @@ app.get('/hello', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log("the application is running on localhost:3000");
-    
+    console.log("the application is running on localhost:3000");    
 });
