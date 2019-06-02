0a8cdfe549c46534e930e37dfbdfcdea8ffee67c
chapmanCC <chapboy1989@gmail.com>
2019-06-03 01:22:19 +0800

    complete second section

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 
 app.set('view engine', 'pug');
@@ -8,8 +7,16 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.get('/hello', (req, res) => {
-    res.send("<h1>Hello World</h1>");
+// app.get('/hello', (req, res) => {
+//     res.send("<h1>Hello World</h1>");
+// })
+
+app.get('/card', (req, res) => {
+    // res.locals.prompt = "Who is buried in Grant's Tomb?"
+    res.render('card', {
+        prompt: "Who is buried in Grant's Tomb?", 
+        hint: "Think about whose tomb it is."
+    });
 })
 
 app.listen(3000, () => {
