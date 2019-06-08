3bf8978430f311ed8e9129e67f59bc8a255c35d3
chapmanCC <chapboy1989@gmail.com>
2019-06-09 03:53:40 +0800

    complete 4

--- app.js
+++ app.js
@@ -1,24 +1,101 @@
-const express = require('express');
+// * ----------
+// * REQUIRE
+// * ----------
+
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+// * ----------
+// * USE
+// * ----------
 
-// app.get('/hello', (req, res) => {
-//     res.send("<h1>Hello World</h1>");
-// })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get('/card', (req, res) => {
+app.use((req, res, next) => {
+    console.log("Hello");
+    const err = new Error("Oh no");
+    err.status = 500;
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("World");
+    next();
+});
+// * ----------
+// * SETTINGS
+// * ----------
+
+app.set("view engine", "pug");
+
+// * ----------
+// * PAGES
+// * ----------
+
+// * root
+app.get("/", (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.render("index", { name });
+    } else {
+        res.redirect("/hello");
+    }
+});
+
+// * /card
+app.get("/card", (req, res) => {
     // res.locals.prompt = "Who is buried in Grant's Tomb?"
-    res.render('card', {
-        prompt: "Who is buried in Grant's Tomb?", 
+    res.render("card", {
+        prompt: "Who is buried in Grant's Tomb?",
         hint: "Think about whose tomb it is."
     });
-})
+});
+
+// * /hello
+app.get("/hello", (req, res) => {
+    const name = req.cookies.username;
+
+    if (name) {
+        res.redirect("/");
+    } else {
+        res.render("hello");
+    }
+});
+app.post("/hello", (req, res) => {
+    res.cookie("username", req.body.username);
+    res.redirect("/");
+});
+
+// * /goodbye
+app.post("/goodbye", (req, res) => {
+    res.clearCookie("username");
+    res.redirect("/hello");
+});
+
+app.use((req, res, next) => {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
+// * ----------
+// * ERROR HANDLER
+// * ----------
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render("error");
+    next();
+});
+
+// * ----------
+// * LISTENER
+// * ----------
 
 app.listen(3000, () => {
-    console.log("the application is running on localhost:3000");    
+    console.log("the application is running on localhost:3000");
 });
